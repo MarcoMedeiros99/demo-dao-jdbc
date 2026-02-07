186571227d35659663d5f31efd874eae3ebe2c8e
MarcoMedeiros99 <marcomed07@gmail.com>
2026-02-06 22:57:09 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,25 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
 
 		Department obj = new Department(1, "book");
+		Seller seller = new Seller(1, "marco", "marco@gmail.com", new Date(), 2.650, obj);
+		
 		System.out.println(obj);
-
+		System.out.println(seller);
+		
+		
+		
 	}
 
 }
