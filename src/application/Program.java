dc78abad01b1703519a85cb4559c77ec29e7a856
MarcoMedeiros99 <marcomed07@gmail.com>
2026-02-07 23:02:12 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,9 @@
 package application;
 
-import java.util.Date;
 import java.util.Locale;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
@@ -11,15 +11,11 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		
-
-		Department obj = new Department(1, "book");
-		Seller seller = new Seller(1, "marco", "marco@gmail.com", new Date(), 2.650, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj);
-		System.out.println(seller);
+		Seller seller = sellerDao.findById(9);
 		
-		
-		
+		System.out.println(seller);		
 	}
 
 }
